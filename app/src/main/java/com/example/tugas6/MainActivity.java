54f112b011d4d3304ca36417a99bddf84695aa05
Seqyaa <nickolamauregar@gmail.com>
2022-11-26 17:35:52 +0700

    update

--- app/src/main/java/com/example/tugas6/MainActivity.java
+++ app/src/main/java/com/example/tugas6/MainActivity.java
@@ -7,38 +7,52 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import com.google.android.material.navigation.NavigationView;
-
-// tugas recycle view
+import androidx.work.ExistingWorkPolicy;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import com.example.tugas6.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     // tugas recycle view
     RecyclerView recylerView;
-    String s1[], s2[],s3[];
-    int images[] = {R.drawable.images,R.drawable.sepatu,R.drawable.foto};
+    String s1[], s2[], s3[];
+    int images[] = {R.drawable.images, R.drawable.sepatu, R.drawable.foto};
 
     private DrawerLayout dl;
     private ActionBarDrawerToggle abut;
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // tugas recycle view
+        // tugas MyWorker
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot()); final OneTimeWorkRequest request = new
+                OneTimeWorkRequest.Builder(MyWorker.class).build(); binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                WorkManager.getInstance().enqueueUniqueWork("Notifikasi", ExistingWorkPolicy.REPLACE, request);
+            }
+        });
+
+
+    // tugas recycle view
+
         recylerView = findViewById(R.id.recyclerView);
         s1 = getResources().getStringArray(R.array.Sepatu);
         s2 = getResources().getStringArray(R.array.star);
         s3 = getResources().getStringArray(R.array.harga);
         SepatuAdapter appAdapter = new SepatuAdapter(this, s1, s2, s3, images);
         recylerView.setAdapter(appAdapter);
-        LinearLayoutManager layoutManager  = new LinearLayoutManager(MainActivity.this,LinearLayoutManager.HORIZONTAL,false);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false);
         recylerView.setLayoutManager(layoutManager);
         recylerView.setItemAnimator(new DefaultItemAnimator());
-
-
-
         dl = (DrawerLayout) findViewById(R.id.dl);
         abut = new ActionBarDrawerToggle(this, dl, R.string.Open, R.string.Close);
         abut.setDrawerIndicatorEnabled(true);
@@ -64,12 +78,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        return abut.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+        public boolean onOptionsItemSelected (@NonNull MenuItem item){
+            return abut.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+        }
     }
-}
+
 
 
 
